e71b9a86fbc61df5102d3c04ef8fc707f2cf5c98
fjosea10 <fjosea10@gmail.com>
2023-11-27 18:37:51 -0600

    user can now log in and will only be redirected to AllRecipes if successful login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+// import './App.css';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Import Navigate
+// import React, { useState } from 'react';
+// import RecipeForm from './RecipeForm.js';
+// import AllRecipes from './AllRecipes.js';
+// import UserRecipes from './UserRecipes.js';
+// import ManageStudents from './ManageStudents.js';
+// import Login from './Login';
+
+// const App = () => {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   const handleLogin = () => {
+//     setIsLoggedIn(true);
+//   };
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route 
+//           path='/login' 
+//           element={<Login onLogin={handleLogin} />}
+//         />
+//         <Route
+//           path='/'
+//           element={isLoggedIn ? <AllRecipes /> : <Navigate to="/login" />}
+//         />
+//         <Route path='/RecipeForm' element={<RecipeForm />} />
+//         <Route path='/AllRecipes' element={<AllRecipes />} />
+//         <Route path='/ManageStudents' element={<ManageStudents />} />
+//         <Route path='/UserRecipes' element={<UserRecipes />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+// //////////////////////////////////////////////////////////////////
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Import Navigate
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import RecipeForm from './RecipeForm.js';
 import AllRecipes from './AllRecipes.js';
@@ -25,8 +64,8 @@ const App = () => {
           path='/'
           element={isLoggedIn ? <AllRecipes /> : <Navigate to="/login" />}
         />
-        <Route path='/RecipeForm' element={<RecipeForm />} />
         <Route path='/AllRecipes' element={<AllRecipes />} />
+        <Route path='/RecipeForm' element={<RecipeForm />} />
         <Route path='/ManageStudents' element={<ManageStudents />} />
         <Route path='/UserRecipes' element={<UserRecipes />} />
       </Routes>
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,93 @@
+// import React, { useState } from 'react';
+// import './Login.css';
+
+// const Login = ({ onLogin } ) => {
+//   const [loginData, setLoginData] = useState({
+//     username: '',
+//     password: '',
+//   });
+
+//   const handleLogin = async (event) => {
+//     event.preventDefault();
+//     try {
+//       const response = await fetch('http://localhost:80/nutrition-user/login', {
+//         method: 'POST',
+//         headers: {
+//           Authorization: 'Basic ' + btoa(`${loginData.username}:${loginData.password}`)
+//         },
+//       }
+//       );
+
+//       const data = await response.json();
+//       console.log('Success: ', data);
+//       // Do something with the successful response data
+
+//       onLogin();
+
+//     } catch (error) {
+//       console.log(error);
+//       // Handle any errors that occurred during the fetch or processing of the response
+//     }
+//   }; // <- Add a closing parenthesis here
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setLoginData({
+//       ...loginData,
+//       [name]: value,
+//     });
+//   };
+
+//   return (
+//     <div className="login-container">
+//       <h1>Login</h1>
+//       <form onSubmit={handleLogin} className="login-form">
+//         <div>
+//           <label htmlFor="username">Username:</label> <br />
+//           <input
+//             type="text"
+//             id="username"
+//             name="username"
+//             value={loginData.username}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor="password">Password:</label> <br />
+//           <input
+//             type="password"
+//             id="password"
+//             name="password"
+//             value={loginData.password}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <button type="submit" className="login-button" onClick={handleLogin}>
+//           Login
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+
+
+////////////////////////////////////////////////////////////////
+
 import React, { useState } from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
-const Login = ({ onLogin } ) => {
+const Login = ({ onLogin }) => {
   const [loginData, setLoginData] = useState({
     username: '',
     password: '',
   });
+
+  // Use useNavigate to get the navigation function
+  const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -15,20 +97,29 @@ const Login = ({ onLogin } ) => {
         headers: {
           Authorization: 'Basic ' + btoa(`${loginData.username}:${loginData.password}`)
         },
-      }
-      );
+      });
 
       const data = await response.json();
+      if (response.ok){
       console.log('Success: ', data);
-      // Do something with the successful response data
 
+      // Call onLogin when authentication is successful
       onLogin();
-
+      
+      // Redirect to the desired route (e.g., /RecipeForm)
+      navigate('/AllRecipes');
+      } else {
+        console.error(
+          'Failed to log in:',
+          response.status,
+          response.statusText
+        );
+      }
     } catch (error) {
-      console.log(error);
+      console.log('Error logging in: ', error);
       // Handle any errors that occurred during the fetch or processing of the response
     }
-  }; // <- Add a closing parenthesis here
+  };
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -62,7 +153,7 @@ const Login = ({ onLogin } ) => {
             onChange={handleInputChange}
           />
         </div>
-        <button type="submit" className="login-button" onClick={handleLogin}>
+        <button type="submit" className="login-button">
           Login
         </button>
       </form>
@@ -71,3 +162,4 @@ const Login = ({ onLogin } ) => {
 };
 
 export default Login;
+
