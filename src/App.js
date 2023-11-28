f2352571333435c9b0978eb09d2d7705caafc132
fjosea10 <fjosea10@gmail.com>
2023-11-28 01:17:53 -0600

    enhanced login with Routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import RequireAuth from './RequireAuth.js';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import RecipeForm from './RecipeForm.js';
@@ -7,36 +8,73 @@ import UserRecipes from './UserRecipes.js';
 import ManageStudents from './ManageStudents.js';
 import Login from './Login';
 import ViewStudents from './ViewStudents.js';
+import Layout from './Layout.js';
+import Unauthorized from './Unauthorized.js';
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const ROLES = {
+  "admin": "admin",
+  "student": "student"
+}
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route 
-          path='/login' 
-          element={<Login onLogin={handleLogin} />}
-        />
-        <Route
-          path='/'
-          element={isLoggedIn ? <AllRecipes /> : <Navigate to="/login" />}
-        />
-        <Route path='/AllRecipes' element={<AllRecipes />} />
-        <Route path='/RecipeForm' element={<RecipeForm />} />
-        <Route path='/ManageStudents' element={<ManageStudents />} />
-        <Route path='/UserRecipes' element={<UserRecipes />} />
-        <Route path='/ViewStudents' element={<ViewStudents />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path='login' element={<Login/>} />
+        <Route path='' element={<Login/>} />
+        <Route path='unauthorized' element={<Unauthorized/>} />
+        
+
+        {/* protected routes */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.admin, ROLES.student]} />}>     
+          <Route path='AllRecipes' element={<AllRecipes />} />
+          <Route path='RecipeForm' element={<RecipeForm />} />
+          <Route path='UserRecipes' element={<UserRecipes />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.admin ]} />}>     
+          <Route path='ManageStudents' element={<ManageStudents />} />
+          <Route path='ViewStudents' element={<ViewStudents />} />
+        </Route>
+
+      </Route>
+    </Routes>
   );
-};
+}
 
 export default App;
+
+
+// const App = () => {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   const handleLogin = () => {
+//     setIsLoggedIn(true);
+//   };
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route 
+//           path='/login' 
+//           element={<Login onLogin={handleLogin} />}
+//         />
+//         <Route
+//           path='/'
+//           element={isLoggedIn ? <AllRecipes /> : <Navigate to="/login" />}
+//         />
+//         <Route path='/AllRecipes' element={<AllRecipes />} />
+//         <Route path='/RecipeForm' element={<RecipeForm />} />
+//         <Route path='/ManageStudents' element={<ManageStudents />} />
+//         <Route path='/UserRecipes' element={<UserRecipes />} />
+//         <Route path='/ViewStudents' element={<ViewStudents />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
 
 
 // //////////////////////////////////////////////////////////////////
